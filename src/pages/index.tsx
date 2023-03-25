58f4c46521e890f18a3aed1059fdd7acf399b0b3
daviramosds <davirds.dev@gmail.com>
2023-03-25 15:28:07 -0300

    project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return ( 
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-zinc-700 text-2xl">Hello, world!</h1>
+    </div>
+  )
+}
