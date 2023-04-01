a4e0b69f52f915a46655ef792badad96a47973b4
daviramosds <davirds.dev@gmail.com>
2023-04-01 12:59:22 -0300

    add: username

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,7 @@ interface ProfileProps {
 
 export function Profile({ src }: ProfileProps) {
   return (
-    <div className="w-36 h-36 rounded-full border-2 border-blue-600 p-1">
+    <div className="w-36 h-36 rounded-full border-2 border-blue-600 p-1 text-center">
       <div className="w-full h-full relative">
         <Image
           className="rounded-full"
@@ -15,6 +15,8 @@ export function Profile({ src }: ProfileProps) {
           alt={"profile image"}
         />
       </div>
+
+      <h4 className="text-blue-600 text-lg my-3">@username</h4>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Profile } from "@/components/Profile";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full h-screen flex items-center justify-center flex-col">
       <Profile src="https://github.com/daviramosds.png" />
     </div>
   );
