c2b6f7faf254e544876075de9fedfc2c667f34ff
daviramosds <davirds.dev@gmail.com>
2023-04-02 10:06:27 -0300

    add: dark variation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Profile } from "@/components/Profile";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center flex-col gap-6">
+    <div className="w-full h-screen flex items-center justify-center flex-col gap-6   dark: bg-slate-900">
       <Profile image="https://github.com/daviramosds.png" redirect_url="https://github.com/daviramosds" username="@daviramosds" />
       <Card src="https://github.com/daviramosds" title="github" />
     </div>
