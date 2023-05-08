2b0f239a647c3bc51f4cf4036714be257f845269
daviramosds <davirds.dev@gmail.com>
2023-05-08 18:44:34 -0300

    add: apollo and connect to hygraph

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,34 @@
 import { Card } from "@/components/Card";
 import { Profile } from "@/components/Profile";
 import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+import { client } from "@/lib/apollo";
+import { gql } from "@apollo/client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [items, setItems] = useState([{}])
+
+  const fetchApiData = async () => {
+    const { data } = await client.query({
+      query: gql`
+        query Assets {
+          links {
+            id
+            slug
+            title
+            url
+          }
+        }
+      `,
+    });
+
+    setItems(data.links)
+  };
+
+  useEffect(() => {
+    fetchApiData()
+  }, [])
 
   return (
     <div className="w-full h-screen flex items-center justify-center flex-col gap-6 dark:bg-slate-900">
@@ -14,6 +40,7 @@ export default function Home() {
       <Card src="https://github.com/daviramosds" title="github" />
 
       <ThemeSwitcher />
+      <button onClick={fetchApiData}>Test</button>
     </div>
   );
 }
